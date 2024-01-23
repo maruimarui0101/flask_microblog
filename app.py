8e6dda26dcc993c83c281dcdeaaaa489997b77f7
Casey M <cd.manalansan@gmail.com>
2024-01-23 16:26:37 +0000

    adds additional date formatting to posts

--- app.py
+++ app.py
@@ -12,4 +12,14 @@ def home():
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
         entries.append((entry_content, formatted_date))
-    return render_template("home.html", entries=entries)
+
+    entries_with_date = [
+        (
+            entry[0],
+            entry[1],
+            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d"),
+        )
+        for entry in entries
+    ]
+
+    return render_template("home.html", entries=entries_with_date)
