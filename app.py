e9f938f128df61f9cefb1fa67819a8fe269fec89
Casey M <cd.manalansan@gmail.com>
2024-01-23 01:00:57 +0000

    adds GET and POST methods to home function with form content obtained

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        entry_content = request.form.get("content")
     return render_template("home.html")
