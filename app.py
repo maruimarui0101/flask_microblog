f0800107c030d134ae088d3864547ecbf0d079d2
Casey M <cd.manalansan@gmail.com>
2024-01-23 16:22:24 +0000

    persists microblog posts into list element temporarily.

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+entries = []
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         entry_content = request.form.get("content")
-        print(entry_content, datetime.datetime.today())
-    return render_template("home.html")
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        entries.append((entry_content, formatted_date))
+    return render_template("home.html", entries=entries)
